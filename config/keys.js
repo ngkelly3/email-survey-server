7ec4a596a88a2d2c08f1872052899d01eaa3198e
Kelly Ng <kellyng87@gmail.com>
2017-12-14 20:12:09 -0800

    implement logout

--- config/keys.js
+++ config/keys.js
@@ -1,5 +1,6 @@
 module.exports = {
   googleClientID: '927685171919-tha8c8rrvg6tnbijalr98cpe9ml1co7u.apps.googleusercontent.com',
   googleClientSecret: 'PtkDR2Gs4ENdcBDuIJBxvG3G',
-  mongoURI: 'mongodb://kelly:password@ds139262.mlab.com:39262/emaily-dev-kelly'
+  mongoURI: 'mongodb://kelly:password@ds139262.mlab.com:39262/emaily-dev-kelly',
+  cookieKey: ';sdafj;asjfa;sojf'
 };
