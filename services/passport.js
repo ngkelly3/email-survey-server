d65cf1945637ff5d1686dd3691c684367715ac9c
Kelly Ng <kellyng87@gmail.com>
2017-12-14 19:36:36 -0800

    incorporate cookies

--- services/passport.js
+++ services/passport.js
@@ -4,6 +4,20 @@ const mongoose = require('mongoose');
 const keys = require('../config/keys.js');
 
 const User = mongoose.model('users');
+
+passport.serializeUser((user, done)=> {
+  // user is what we got from DB
+  // works with multiple passport strategies
+  done(null, user.id); //mongoID -> very easily use multiple auth providers
+
+});
+
+passport.deserializeUser((id, done) => {
+  User.findById(id)
+    .then(user => {
+      done(null, user);
+    })
+})
 // clientID: 927685171919-tha8c8rrvg6tnbijalr98cpe9ml1co7u.apps.googleusercontent.com
 // clientSecret: PtkDR2Gs4ENdcBDuIJBxvG3G
 passport.use(new GoogleStrategy({
