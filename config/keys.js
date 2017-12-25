b52e3cf24420b5e183a5347e3878c453d5386940
Kelly Ng <kellyng87@gmail.com>
2017-12-24 23:45:25 -0800

    add prod env

--- config/keys.js
+++ config/keys.js
@@ -1,3 +1,11 @@
+if (process.env.NODE_ENV === 'production') {
+  // we are in prod - return the prod set of keys
+  module.exports = require('./prod');
+} else {
+  // we are in dev - return the dev keys!
+  module.exports = require('./dev');
+}
+
 module.exports = {
   googleClientID: '927685171919-tha8c8rrvg6tnbijalr98cpe9ml1co7u.apps.googleusercontent.com',
   googleClientSecret: 'PtkDR2Gs4ENdcBDuIJBxvG3G',
