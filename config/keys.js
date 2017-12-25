9cff90c5a8ce1568a046fdbeccedefb31ed75636
Kelly Ng <kellyng87@gmail.com>
2017-12-25 00:38:27 -0800

    testing

--- config/keys.js
+++ config/keys.js
@@ -1,14 +1,9 @@
 if (process.env.NODE_ENV === 'production') {
   // we are in prod - return the prod set of keys
+  console.log("Hit production");
   module.exports = require('./prod');
 } else {
   // we are in dev - return the dev keys!
+  console.log("hit dev")
   module.exports = require('./dev');
 }
-
-module.exports = {
-  googleClientID: '927685171919-tha8c8rrvg6tnbijalr98cpe9ml1co7u.apps.googleusercontent.com',
-  googleClientSecret: 'PtkDR2Gs4ENdcBDuIJBxvG3G',
-  mongoURI: 'mongodb://kelly:password@ds139262.mlab.com:39262/emaily-dev-kelly',
-  cookieKey: ';sdafj;asjfa;sojf'
-};
