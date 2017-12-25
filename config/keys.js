26cc3e9e9b631c7afc08b802b4d77d98c2e8431c
Kelly Ng <kellyng87@gmail.com>
2017-12-25 13:23:43 -0800

    add proxy to client package

--- config/keys.js
+++ config/keys.js
@@ -1,9 +1,7 @@
 if (process.env.NODE_ENV === 'production') {
   // we are in prod - return the prod set of keys
-  console.log("Hit production");
   module.exports = require('./prod');
 } else {
   // we are in dev - return the dev keys!
-  console.log("hit dev")
   module.exports = require('./dev');
 }
